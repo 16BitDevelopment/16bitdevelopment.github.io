ed23317905515eaf72783d2bc44cf257a8079e7b
16BitDevelopment <167261304+16BitDevelopment@users.noreply.github.com>
2024-04-22 18:03:32 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe (el));
+
+function scrollArrow() {
+    document.getElementsByClassName("arrow")[0].style.opacity = 0;
+}
+
+window.addEventListener("scroll", scrollArrow);
+
+function clipboard() {
+    navigator.clipboard.writeText("16bitdevelopment@gmail.com");
+    document.getElementById("copy").style.opacity = 1;
+}
